e8320e901af7c6ee445843953a1cb2b4d4f05e9b
Yegor Bugayenko <yegor@tpc2.com>
2012-08-24 16:01:24 +0200

    typo

--- s3auth-rest/src/main/java/com/s3auth/rest/AuthRs.java
+++ s3auth-rest/src/main/java/com/s3auth/rest/AuthRs.java
@@ -39,7 +39,6 @@ import com.s3auth.hosts.User;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
