7ca3e1652af17bf9f3e68b78fff0b05f56758c04
Yegor Bugayenko <yegor@tpc2.com>
2012-09-28 17:31:20 +0200

    #18: scope changes reverted back

--- s3auth-rest/src/main/java/com/s3auth/rest/AuthRs.java
+++ s3auth-rest/src/main/java/com/s3auth/rest/AuthRs.java
@@ -69,7 +69,6 @@ public final class AuthRs extends BaseRs {
                 .fromUri("https://www.facebook.com/dialog/oauth")
                 .queryParam("client_id", "{id}")
                 .queryParam("redirect_uri", "{uri}")
-                .queryParam("scope", "manage_pages")
                 .build(
                     Manifests.read("S3Auth-FbId"),
                     this.redirectUri()
@@ -152,7 +151,6 @@ public final class AuthRs extends BaseRs {
             .queryParam("client_id", "{id}")
             .queryParam("redirect_uri", "{uri}")
             .queryParam("client_secret", "{secret}")
-            .queryParam("scope", "manage_pages")
             .queryParam("code", "{code}")
             .build(
                 Manifests.read("S3Auth-FbId"),
