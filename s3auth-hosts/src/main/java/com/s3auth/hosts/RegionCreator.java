6fc217d67a14980af97956f4e6d3789fd856ca49
Dmitri Pisarenko <dp@altruix.co>
2014-12-28 20:14:51 +0300

    Attempt at fixing the problem [ERROR] \src\main\java\com\s3auth\hosts\DefaultDynamo.java[30]: Lines in file should end with Unix-like end of line (RegexpMultilineCheck).

--- s3auth-hosts/src/main/java/com/s3auth/hosts/RegionCreator.java
+++ s3auth-hosts/src/main/java/com/s3auth/hosts/RegionCreator.java
@@ -29,7 +29,7 @@
  */
 package com.s3auth.hosts;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+i mport com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.jcabi.aspects.Immutable;
 import com.jcabi.dynamo.Region;
 
