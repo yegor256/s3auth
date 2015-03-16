f9063abe0b239cc41c9f5bc698a9af4a2b803ea7
Yegor Bugayenko <yegor@tpc2.com>
2015-03-16 11:55:46 -0700

    #250 NPE eliminated

--- s3auth-relay/src/test/java/com/s3auth/relay/HttpExceptionTest.java
+++ s3auth-relay/src/test/java/com/s3auth/relay/HttpExceptionTest.java
@@ -29,6 +29,7 @@
  */
 package com.s3auth.relay;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -54,6 +55,22 @@ public final class HttpExceptionTest {
         MatcherAssert.assertThat(
             HttpResponseMocker.toString(exp.response()),
             Matchers.startsWith("HTTP/1.1 404 Not Found")
+        );
+    }
+
+    /**
+     * HttpException can be instantiated with a NULL inside exception.
+     * @throws Exception If there is some problem inside
+     */
+    @Test
+    public void makesHttpResponseWithNull() throws Exception {
+        final HttpException exp = new HttpException(
+            HttpURLConnection.HTTP_SEE_OTHER,
+            new IOException((String) null)
+        );
+        MatcherAssert.assertThat(
+            HttpResponseMocker.toString(exp.response()),
+            Matchers.startsWith("HTTP/1.1 303 See Other")
         );
     }
 
