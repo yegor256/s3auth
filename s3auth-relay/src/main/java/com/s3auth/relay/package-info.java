df692c462e5a6bb2a3defd50f1e7992725591782
Yegor Bugayenko <yegor256@gmail.com>
2022-07-29 06:38:11 +0300

    #400 year up

--- s3auth-relay/src/main/java/com/s3auth/relay/package-info.java
+++ s3auth-relay/src/main/java/com/s3auth/relay/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012-2019, s3auth.com
+ * Copyright (c) 2012-2022, Yegor Bugayenko
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
